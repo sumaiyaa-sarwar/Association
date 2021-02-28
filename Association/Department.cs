239e5c280cff2ea65dc8efca18e683a9c8e975a3
sumaiyaa-sarwar <19-40319-1@student.aiub.edu>
2021-02-28 16:15:43 +0600

    My home task

---
+++ Association/Department.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Association
+{
+    class Department
+    {
+        private string name;
+
+            public string Name
+        {
+            get 
+            {
+                return Name;
+            }
+            set
+            {
+                Name = value;
+            }
+            private string id;
+        public string id
+        {
+            get
+            {
+                return Value;
+
+            }
+            set
+            {
+                id = value;
+            }
+
+            public Department() { }
+
+        public Department (string name, string Id)
+         {
+            this.name = name;
+            this.id = Id;
+         }
+
+        public void ShowDetails()
+             {
+            Console.WriteLine("Name : +name");
+            Console.WriteLine("Id : +id");
+
+            }
+         }
+
+        }
+    
